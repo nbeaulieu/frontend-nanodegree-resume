bb581b2b85112e024bb19ddf9245b06fee365c57
Nicole Beaulieu <nbeaulieu@me.com>
2014-12-15 05:02:06 -0800

    Add objects for page.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,85 @@
+var bio = {
+    "name" : "Nicole Beaulieu",
+    "role" : "Computer Scientist",
+    "welcomeMessage" : "Geek Greetings",
+    "contacts": {
+        "mobile": "775.303.6384",
+        "email": "nbeaulieu@me.com",
+        "github": "nbeaulieu",
+        "twitter": "",
+        "location": "Reno, NV"
+    }
+}
+
+var education = [
+    {
+        "schools": [
+            {
+                "name": "University of San Diego",
+                "city": "San Diego, CA",
+                "degree": "Bachelor of Arts",
+                "majors": [
+                    "Bachelors, Computer Science"
+                ],
+                "dates": "1989-1993",
+                "url": "http://www.sandiego.edu/"
+            },
+            {
+                "name": "Iowa State University",
+                "city": "Iowa City, IA",
+                "degree": "Master of Science",
+                "majors": [
+                    "Human Computer Interaction"
+                ],
+                "dates": "2011-present",
+                "online": "http://www.uiowa.edu/"
+            }
+        ]
+    },
+    {
+        "onlineCourses": [
+            {
+                "title": "Front End Developer Nanodegree",
+                "school": "Udacity",
+                "dates": "2014-2015",
+                "url": "http://www.udacity.com"
+            }
+        ]
+    }
+]
+
+var work =  {
+    "jobs": [
+        {
+            "employer": "j1",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        },
+        {
+            "employer": "j2",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "p1",
+            "dates": "",
+            "description": "",
+            "image": ""
+        },
+        {
+            "title": "p1",
+            "dates": "",
+            "description": "",
+            "image": ""
+        }
+    ]
+}
